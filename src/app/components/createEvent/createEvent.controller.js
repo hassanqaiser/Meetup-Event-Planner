2fd4e7ae3ba6352b03429fb262d3e3c7c7a21646
Qaiser Hassan <qaiser43@outlook.com>
2016-07-31 10:12:26 +0300

    Fixes for Review # 2
    Fixes for Review # 2

--- src/app/components/createEvent/createEvent.controller.js
+++ src/app/components/createEvent/createEvent.controller.js
@@ -12,6 +12,7 @@ angular.module('meetupEvents')
     var vm = this;
 
 		$("#eventName").focus();
+
 		vm.location = '';
 
     vm.page = 1;
@@ -26,91 +27,109 @@ angular.module('meetupEvents')
     vm.addGuest = function () {
       vm.guests.push(vm.guest);
       vm.guest = '';
+			$("#guest").removeAttr( "required" );
     };
 
     vm.removeGuest = function (guest) {
       var index = vm.guests.indexOf(guest);
       vm.guests.splice(index, 1);
+			if(vm.guests.length < 1)
+				$("#guest").attr("required",true);
     }
 
+	  vm.searchVenue = function(){
+			if (navigator.geolocation) {
+			    navigator.geolocation.getCurrentPosition(function (position) {
 
+	        vm.localLat = position.coords.latitude;
+	        vm.localLng = position.coords.longitude;
+					var apiUriParams = {
+			      query: vm.location,
+						client_id: FOURSQURE_API.CLIENT_ID,
+						client_secret: FOURSQURE_API.CILENT_SECRET,
+						v: FOURSQURE_API.VERSION,
+						ll: vm.localLat + "," + vm.localLng
+					};
 
-  vm.searchVenue = function(){
-		if (navigator.geolocation) {
-		    navigator.geolocation.getCurrentPosition(function (position) {
+			    var apiUri = Object.keys(apiUriParams).map(function (key) {
+						return encodeURIComponent(key) + '=' + encodeURIComponent(apiUriParams[key]);
+					}).join('&');
 
-        vm.localLat = position.coords.latitude;
-        vm.localLng = position.coords.longitude;
-				var apiUriParams = {
-		      query: vm.location,
-					client_id: FOURSQURE_API.CLIENT_ID,
-					client_secret: FOURSQURE_API.CILENT_SECRET,
-					v: FOURSQURE_API.VERSION,
-					ll: vm.localLat + "," + vm.localLng
-				};
-
-		    var apiUri = Object.keys(apiUriParams).map(function (key) {
-					return encodeURIComponent(key) + '=' + encodeURIComponent(apiUriParams[key]);
-				}).join('&');
-
-		    $http.get(FOURSQURE_API.URL + apiUri)
-					.success(function (data, statusCode) {
-						// Successfull
-						if (statusCode == 200) {
-							vm.places = data.response.venues;
-							// Loops through the venues to measure the distance from local location
-							for (var i = 0; i < vm.places.length; i++) {
-								vm.places[i].distance = distanceCalculator.get(vm.localLat, vm.localLng, vm.places[i].location.lat, vm.places[i].location.lng);
+			    $http.get(FOURSQURE_API.URL + apiUri)
+						.success(function (data, statusCode) {
+							// Successfull
+							if (statusCode == 200) {
+								vm.places = data.response.venues;
+								// Loops through the venues to measure the distance from local location
+								for (var i = 0; i < vm.places.length; i++) {
+									vm.places[i].distance = distanceCalculator.get(vm.localLat, vm.localLng, vm.places[i].location.lat, vm.places[i].location.lng);
+								}
 							}
-						}
 
-						// No result found!
-						if (vm.places.length == 0) {
-							vm.ResponseDetails = "No result found! Try (ex. Nordea, Burger, Coffee )";
-						}
-					})
-					.error(function (data, status) {
-						vm.ResponseDetails = "Bad Request:  " + status;
-		        vm.places = [];
-		        vm.totalRecordsCount = 0;
-					});
-		},
-    function (error) {
-      alert('Please share your location to get venue suggestions');
-    }
-  );
-	}
-
-
-  }
-  $scope.$watch('vm.location', function (newValue, oldValue) {
-		if (newValue != null) {
-				vm.searchVenue();
+							// No result found!
+							if (vm.places.length == 0) {
+								vm.ResponseDetails = "No result found! Try (ex. Nordea, Burger, Coffee )";
+							}
+						})
+						.error(function (data, status) {
+							vm.ResponseDetails = "Bad Request:  " + status;
+			        vm.places = [];
+			        vm.totalRecordsCount = 0;
+						});
+			},
+	    function (error) {
+	      alert('Please share your location to get venue suggestions');
+	    }
+	  );
 		}
-	});
 
-	vm.createEvent = function(){
-		events.add({
-			eventname: vm.eventName,
-			eventtype: vm.eventType,
-			host: vm.hostedBy,
-			start: vm.startDate.toString().substring(0, 21),
-			finish: vm.endDate.toString().substring(0, 21),
-			guests: vm.guests,
-			location: vm.location,
-			description: optional(vm.description)
+
+	  }
+
+	  $scope.$watch('vm.location', function (newValue, oldValue) {
+			if (newValue != null) {
+					vm.searchVenue();
+			}
 		});
 
-		function optional(field) {
-			if (field) {
-				return field;
-			} else {
-				return null;
+		$scope.$watch('vm.startDate', checkDates);
+		$scope.$watch('vm.endDate', checkDates);
+
+		function checkDates() {
+			var now = new Date();
+			var endDate = new Date(vm.endDate);
+			var startDate = new Date(vm.startDate);
+
+	    if (vm.startDate) {
+				$scope.createEventForm.startDate.$setValidity("startBeforeNow", startDate >= now);
+	    }
+			if (vm.endDate) {
+				$scope.createEventForm.endDate.$setValidity("endBeforeOrOnStart", endDate > startDate);
 			}
 		}
 
-		$state.go('home');
+		vm.createEvent = function(){
+			events.add({
+				eventname: vm.eventName,
+				eventtype: vm.eventType,
+				host: vm.hostedBy,
+				start: vm.startDate.toString().substring(0, 21),
+				finish: vm.endDate.toString().substring(0, 21),
+				guests: vm.guests,
+				location: vm.location,
+				description: optional(vm.description)
+			});
 
-	};
+			function optional(field) {
+				if (field) {
+					return field;
+				} else {
+					return null;
+				}
+			}
+
+			$state.go('home');
+
+		};
 
   }]);
